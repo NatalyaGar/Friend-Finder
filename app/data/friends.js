b867bc09161d6d6aaa3212db380cbecfd1c0c00d
NatalyaGar <natalya.garusova@gmail.com>
2018-05-01 16:00:48 -0600

    app

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+// Variable to hold the array of friends objects data
+var friends = [{ "name": "https://images.unsplash.com/photo-1509717998015-9f6b71638119?ixlib=rb-0.3.5&q=85&fm=", "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] }, 
+{ "name": "Ninell", "photo": "https://images.unsplash.com/photo-1495681918301-cb317d07d170?ixlib=rb-0.3.5&q=85&f", "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] }, 
+{ "name": "Alex", "photo": "https://images.unsplash.com/photo-1500060296907-5bd5a5482ff2?ixlib=rb-0.3.5&q=85&fm", "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] }, 
+{ "name": "Algella", "photo": "https://images.unsplash.com/photo-1509047195427-622e84a66b4b?ixlib=rb-0.3.5&q=85&f", "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] }, 
+{ "name": "Bob", "photo": "https://images.unsplash.com/photo-1519747652055-21b91f96e5ec?ixlib=rb-0.3.5&q=85&f", "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] },
+{ "name": "Paul", "photo": "https://images.unsplash.com/photo-1499530403357-0ea994f8655a?ixlib=rb-0.3.5&q=85&fm", "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] }];
+
+// Export the data
+module.exports = friends;
